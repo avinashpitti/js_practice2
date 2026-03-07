0eb9b4660293055f5c42af013c425de94e9117b2
avinashpitti <avinash87129@gmail.com>
2026-03-07 18:00:48 +0530

    variables in js

---
+++ variables/variable.js
@@ -0,0 +1,19 @@
+var b=3;
+console.log(b)
+var b=5; // you can redeclare and reassign value
+console.log(b)
+
+
+let a=23;
+console.log(a)
+a=34; // you can reassign but not redeclare
+console.log(a);
+a=12;
+console.log(a);
+
+const age=21;
+console.log(age);
+// you can neither reassign nor redeclare
+
+
+
