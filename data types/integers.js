20754daeaab20fd86063dab9f9e6ad28f07002f5
avinashpitti <avinash87129@gmail.com>
2026-03-09 09:05:59 +0530

    integer datatype

---
+++ data types/integers.js
@@ -0,0 +1,30 @@
+let a=23;
+let b=12.4;
+console.log(typeof(a)) // number
+console.log(typeof(b)) // number
+console.log(a) // 23
+console.log(b) // 12.4
+
+// Arithmetic operations with javascript
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b) // remainder
+
+let x=5;
+x++
+console.log(`The value of x is ${x}`)
+
+let y=10;
+y--
+console.log(`The value of y is ${y}`)
+
+// If you try invalid math, javascript returns NaN
+text='hello'
+console.log(text*4) // NaN
+console.log('text'*4) // NaN
+
+
+
+
