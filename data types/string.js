8b0bf47fee77863750d9f38cc1d41f42caf550d5
avinashpitti <avinash87129@gmail.com>
2026-03-09 08:22:47 +0530

    sting datatype

---
+++ data types/string.js
@@ -0,0 +1,17 @@
+let myName='avinash'
+console.log(myName)
+
+let course=`Javascript`
+console.log(course)
+console.log(course.length)
+
+console.log(`hello ${myName} welcome to ${course}`)
+
+console.log(myName.toUpperCase())
+console.log(myName.includes("na")) // true
+console.log(myName.includes("na ")) // false
+console.log(myName.includes("NA")) // false
+
+let text="    javascript is a scripting language    "
+console.log(text)
+console.log(text.trim())
