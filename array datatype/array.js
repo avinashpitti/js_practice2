9d31e1b506214289d04ab91edce982848d9c97cd
avinashpitti <avinash87129@gmail.com>
2026-03-09 20:09:52 +0530

    array datatype

---
+++ array datatype/array.js
@@ -0,0 +1,23 @@
+let fruits=['mango','banana','orange']
+console.log(fruits)
+console.log(fruits[0])
+console.log(fruits[2])
+console.log(fruits.length)
+
+fruits.push('pineapple','dragon')
+console.log(fruits)
+
+console.log(fruits.pop())
+console.log(fruits)
+
+// unshift() - add at beginning
+fruits.unshift('avacado')
+console.log(fruits)
+
+// shift() - removes first element
+fruits.shift()
+console.log(fruits)
+
+
+
+
