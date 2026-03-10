485af540ea0ad1868e00492af57b66bc840ee9c9
avinashpitti <avinash87129@gmail.com>
2026-03-10 18:51:35 +0530

    logical operators

---
+++ operators/logical.js
@@ -0,0 +1,7 @@
+let country="India"
+let age =34
+
+console.log(age >18 && country=="India") // true
+console.log(age>18 && country=="india") // false
+
+console.log(age>18 || country=='newzealand') // true
