cda4b5cc612c159e41c972587722461b68eaa008
avinashpitti <avinash87129@gmail.com>
2026-03-13 11:51:56 +0530

    for loop practice

---
+++ loops/practice.js
@@ -0,0 +1,67 @@
+// printing numbers in reverse order
+console.log('printing numbers in reverse order')
+for(i=10;i>=1;i--){
+    console.log(i)
+}
+
+//print even numbers
+console.log('printing even numbers')
+for(i=2;i<=20;i+=2){
+    console.log(i);
+}
+
+for(let i=100;i<=120;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+
+// print sum of first 5 numbers
+console.log('sum of first 5 numbers')
+let sum=0;
+for (let i=1;i<=5;i++){
+    sum=sum+i;
+}
+console.log(sum);
+
+//multiplication of 6
+console.log('Multiplication of 6')
+for(let i=1;i<=10;i++){
+    console.log(i*6);
+}
+
+
+//print numbers from 1 to 10
+console.log('print numbers from 1 to 10')
+for (let i=1;i<=10;i++){
+    console.log(i)
+}
+
+// print numbers from 10 to 1
+console.log('print numbers from 10 to 1')
+for (let i=10;i>=1;i--){
+    console.log(i)
+}
+
+// print odd numbers from 1 to 20
+console.log('print odd numbers from 1 to 20')
+for (let i=1;i<=20;i++){
+    if(i%2===1){
+        console.log(i)
+    }
+}
+
+// find sum of numbers from 1 to 100
+console.log('sum of numbers from 1 to 100')
+sum=0;
+for(let i=1;i<=100;i++){
+    sum=sum+i;
+}
+console.log(sum);
+
+//multiplication of 7
+console.log('Multiplication of 7')
+
+for(let i=1;i<=10;i++){
+    console.log(`7*${i}=${7*i}`)
+}
