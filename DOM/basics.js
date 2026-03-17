8d98833161c9884b49a94faad8f9fe75f671465c
avinashpitti <avinash87129@gmail.com>
2026-03-17 12:35:01 +0530

    dom basics

---
+++ DOM/basics.js
@@ -0,0 +1,28 @@
+// DOM : stands for document object model
+// It represents your html page as tree structure of objects
+// It allows js to read, modify,add or delete elements and content
+// on a webpage automatically
+
+// Example HTML
+<body>
+    <h1 id='title'>Hello</h1>
+    <p>Welcome</p>
+</body>
+
+// DOM structure(tree structure)
+// document
+//     ---body
+//         ---h1
+//         ---p (js can access and modify these elements)
+
+
+// selecting elements
+// To work with elements, first we need to select elements
+getElementById()
+getElementsByClassName()
+querySelector()
+querySelectorAll()
+
+// changing content
+innerText
+innerHTML
