4577b7e52ad48f5963e5cf4e8299ab7fd16c1f7f
avinashpitti <avinash87129@gmail.com>
2026-04-01 11:11:26 +0530

    do while loop

--- practice.js
+++ practice.js
@@ -1,22 +1,5 @@
-let numbers=[4,8,3,9,67,22,19,45]
-
-for (let number of numbers){
-    console.log(number);
-}
-
-
-let fruits=['avocado','berries','cherry','dragon']
-
-for (let fruit of fruits){
-    console.log(fruit);
-}
-
-
-let my_name='Avinash'
-
-for (let chars of my_name){
-    console.log(chars);
-}
-
-
-
+let i=10
+do{
+    console.log(i)
+    i++
+}while(i<=5)
