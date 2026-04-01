a3799297379a3621e2304542f9ce49e038aee703
avinashpitti <avinash87129@gmail.com>
2026-04-01 10:02:28 +0530

    switch case statement

---
+++ practice.js
@@ -0,0 +1,25 @@
+let day='Thursday';
+
+switch(day){
+    case 'Monday':
+        console.log('Today is monday');
+        break
+    case 'Tuesday':
+        console.log('Today is Tuesday');
+        break
+    case 'Wednesday':
+        console.log('Today is Wednesday');
+        break
+    case 'Thursday':
+        console.log('Today is Thursday');
+        break
+    case 'Friday':
+        console.log('Today is Friday');
+        break
+    case 'Saturday':
+    case 'Sunday':
+        console.log('Enjoy the weekend')
+        break
+    default:
+        console.log('Enter correct day');
+}
