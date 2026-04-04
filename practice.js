c159217fa0a6a699c13cbfe2678c46ea705d98cb
avinashpitti <avinash87129@gmail.com>
2026-04-04 11:45:52 +0530

    sorting an array

--- practice.js
+++ practice.js
@@ -1,5 +1,12 @@
-let fruits=['apple','orange','banana']
+let fruits=['mango','banana','avocado','apple','berry']
 
-fruits.forEach(fruit=>{
-    console.log(fruit);
-})
+let sorted=fruits.sort()
+console.log(sorted)
+
+// numbers need a compare function
+let nums=[34,7,19,2,44]
+let ascending=nums.sort((a,b)=>(a-b))
+console.log(ascending)
+
+let descending=nums.sort((a,b)=>(b-a))
+console.log(descending)
