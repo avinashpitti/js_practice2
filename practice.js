33bcfc72ddc7ff88828e1414cb4af548cf9d6ef7
avinashpitti <avinash87129@gmail.com>
2026-04-04 12:26:01 +0530

    forEach loop

--- practice.js
+++ practice.js
@@ -1,4 +1,9 @@
 let nums=[1,2,3,4,5]
 
-let total=nums.reduce((sum,price)=>sum+price,0);
-console.log(total)
+nums.forEach(n=>{
+    console.log(n)
+})
+
+nums.forEach(n=>{
+    console.log(n**2)
+})
