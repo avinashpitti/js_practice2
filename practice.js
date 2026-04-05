3e0dacee8c011e610600ae463587bc97b84ec396
avinashpitti <avinash87129@gmail.com>
2026-04-05 12:52:01 +0530

    hasOwnProperty (check if key exists)

--- practice.js
+++ practice.js
@@ -1,15 +1,6 @@
-let source={a:1};
-let target={b:2,c:3};
-
-Object.assign(source,target);
-console.log(target);
 
 let person={name:'Alice',age:21};
-Object.freeze(person);
 
-console.log(person)
-// person.age=22;
-// console.log(age);
+console.log(person.hasOwnProperty('name'))
+console.log(person.hasOwnProperty('city'))
 
-// person.city='chennai';
-// console.log(city);
