9f4690cac4db0c095cf5acbc75a65ef6288d2c02
avinashpitti <avinash87129@gmail.com>
2026-04-05 11:01:05 +0530

    nested object

--- practice.js
+++ practice.js
@@ -1,12 +1,12 @@
 let person={
     name:'Avinash',
     age:21,
-    city:'Bangalore',
-    state:'Karnataka'
+    address:{
+        city:'Bengaluru',
+        pin:500061
+    }
 }
 
-let {name:student_name,age:student_age}=person
-let {country='srilanka'}=person
-
-console.log(student_age)
-console.log(country)
+let {address:{city,pin}}=person
+console.log(city);
+console.log(pin);
